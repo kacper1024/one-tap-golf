ca9341fcddb8e7a3b77713edb8edcc9d3cd009bd
Kacper Niemirowski <kacper.niemirowski@protonmail.com>
2021-05-13 21:14:40 +0200

    Dodano mechanikę trajektorii lotu piłeczki

---
+++ Assets/Scripts/BallControl.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    public float power = 5f;
+
+    public GameObject PointPrefab;
+
+    public GameObject[] Points;
+
+    public int numberOfPoints;
+
+    Rigidbody2D rb;
+
+    LineRenderer lr;
+
+    Vector2 DragStartPos;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        lr = GetComponent<LineRenderer>();
+
+        Points = new GameObject[numberOfPoints];
+        for(int i = 0; i < numberOfPoints; i++)
+        {
+            Points[i] = Instantiate(PointPrefab, transform.position, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DragStartPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector2 DragEndPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 _velocity = (DragEndPos - DragStartPos) * power;
+
+            Vector2[] trajectory = Plot(rb, (Vector2)transform.position, _velocity, 500);
+            
+            lr.positionCount = trajectory.Length;
+
+            Vector3[] positions = new Vector3[trajectory.Length];
+            for(int i = 0; i < trajectory.Length; i++)
+            {
+                positions[i] = trajectory[i];
+            }
+
+            lr.SetPositions(positions);
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            Vector2 DragEndPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 _velocity = (DragEndPos - DragStartPos) * power;
+            rb.velocity = _velocity;
+        }
+    }
+
+    public Vector2[] Plot(Rigidbody2D rigidbody, Vector2 pos, Vector2 velocity, int steps)
+    {
+        Vector2[] results = new Vector2[steps];
+
+        float timestep = Time.fixedDeltaTime / Physics2D.velocityIterations;
+        Vector2 gravityAccel = Physics2D.gravity * rigidbody.gravityScale * timestep * timestep;
+
+        float drag = 1f - timestep * rigidbody.drag;
+        Vector2 moveStep = velocity * timestep;
+
+        for(int i = 0; i < steps; i++)
+        {
+            moveStep += gravityAccel;
+            moveStep *= drag;
+            pos += moveStep;
+            results[i] = pos;
+        }
+
+        return results;
+    }
+}
